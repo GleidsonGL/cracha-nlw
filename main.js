49a1480b5d9da566a2a3742b481368efc8d16d29
Gleidson Eduardo <91274763+GleidsonGL@users.noreply.github.com>
2021-10-21 21:06:56 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const LinksSocialMedia = {
+  github: 'GleidsonGL',
+  youtube: 'UCyL7gH-jWAQIT9pE7nfJjEQ',
+  facebook: 'gleidson.jaire',
+  instagram: 'gleidsonjaire',
+  twitter: 'gleidsonjairi'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of changeSocialMediaLinks.children) {
+    const social = li.getAttribute ('class')
+
+    li.children[0].href = `https://$
+    {social}.com/${LinksSocialMedia
+    [social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos(){
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    userName.textContent = data.name
+    userBio.textContent = data.bio
+    userLink.href = data.html_url
+    userPhoto.src = data.avatar_url
+    userLogin.textContent = data.login
+  })
+
+}
+
+getGitHubProfileInfos()
